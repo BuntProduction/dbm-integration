eb1018e36a36401b2914a3124af6dc44a8634a2b
BuntProduction <buntlicorn@gmail.com>
2024-05-30 12:34:30 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import ThreeColumnSection from './components/ThreeColumnSection';
+import TextSection from './components/TextSection';
+import CookieIcon from './components/CookieIcon';
+import ContactBar from './components/ContactBar';
+import DescriptionSection from "./components/DescriptionSection";
+import Footer from './components/Footer';
+import ContactPage from './components/pages/ContactPage';
 import './App.css';
+import LoadingScreen from './components/LoadingScreen';
+import imageScreen from './img/loadingscreen.png';
+import Realisations from './components/Realisations';
 
-function App() {
+const AppContent = () => {
+  const location = useLocation();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => setLoading(false), 1000);
+  }, [location.pathname]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {loading && <LoadingScreen image={imageScreen} />}
+      <Header />
+      <Routes>
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/realisations" element={<Realisations />} />
+        <Route path="/" element={
+          <>
+            <MainContent />
+            <ThreeColumnSection />
+            <DescriptionSection />
+            <TextSection />
+            <CookieIcon />
+            <ContactBar />
+          </>
+        } />
+      </Routes>
+      <Footer />
     </div>
   );
-}
+};
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const handleStopLoading = () => setLoading(false);
+    setTimeout(() => handleStopLoading(), 1000);
+    window.addEventListener('beforeunload', handleStopLoading);
+    return () => window.removeEventListener('beforeunload', handleStopLoading);
+  }, []);
+
+  return (
+    <Router>
+      {loading && <LoadingScreen image={imageScreen} />}
+      <AppContent />
+    </Router>
+  );
+};
 
 export default App;
